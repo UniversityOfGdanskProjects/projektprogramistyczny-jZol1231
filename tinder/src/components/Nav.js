3ae6b81cd3f2ba7d74bc9338a647eb746c417831
jzolkiewski <j.zolkiewski.193@studms.ug.edu.pl>
2024-01-05 01:02:09 +0100

    style navbar

--- tinder/src/components/Nav.js
+++ tinder/src/components/Nav.js
@@ -1,13 +1,15 @@
-import logo from '../images/tinderLogo.png';
+import whiteLogo from '../images/tinder_logo_white.png';
+import colorLogo from '../images/color-logo-tinder.png';
 
 
-export default function Nav() {
+export default function Nav({ minimal, authToken }) {
 
     return (
         <nav>
-            <div className="logo-container">
-                <img className="logo" src={logo} alt="logo"/>
+            <div className='logo-container'>
+                <img className='logo' src={minimal ? whiteLogo: colorLogo} alt='logo'/>
             </div>
+            {!authToken && <button className='nav-button'>Log in</button>}
         </nav>
     )
 }
--- tinder/src/pages/Home.js
+++ tinder/src/pages/Home.js
@@ -3,20 +3,21 @@ import Nav from '../components/Nav';
 export default function Home() {
 
     const authenticated = true;
+    const minimal = true;
 
     const handleClick = () => {
         console.log('Clicked!');
     };
 
     return (
-    <>
-        <Nav />
+    <div className="overlay">
+        <Nav minimal={minimal} authToken={authenticated}/>
         <div>
             <h1>Swipe right</h1>
             <button className='primary-button' onClick={handleClick}>
                 {authenticated ? 'Signout' : 'Create an account'}
             </button>
         </div>
-    </>
+    </div>
     )
 }
