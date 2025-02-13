62fabe38615f94ad4f25255386168f53af98ca06
jzolkiewski <j.zolkiewski.193@studms.ug.edu.pl>
2025-02-13 16:40:29 +0100

    landing_page dodanie header

---
+++ landing_page/src/components/header/Header.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+
+
+export default function Header() {
+    return (
+    <header className="p-4 shadow-md flex justify-between items-center">
+      <h1 className="text-xl font-bold">Logo</h1>
+      <Navigation />
+    </header>
+  );
+}
