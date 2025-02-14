09ead9841bdbbbef6d37afe9cfb230a6f4897d7f
jzolkiewski <j.zolkiewski.193@studms.ug.edu.pl>
2025-02-14 05:04:16 +0100

    usuwanie drobnych bledow

--- landing_page/src/components/home/Home.js
+++ landing_page/src/components/home/Home.js
@@ -4,8 +4,8 @@ import Hero from '../hero/Hero';
 export default function Home({ onExploreClick }) {
     return (
         <section className="p-8 text-center">
-            <h1 className="text-2xl font-semibold ">Welcome to the digital revolution!</h1>
-            <p className="mt-2 text-gray-600">Your all-in-one solution for great experiences.</p>
+            <h1 className="text-4xl font-semibold ">Welcome to the digital revolution!</h1>
+            <p className="mt-2 text-gray-400">Your all-in-one solution for great experiences.</p>
             <Navigation />
             <Hero onExploreClick={ onExploreClick } />
 
